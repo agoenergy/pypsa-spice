a4bb429e76078e566074b4f45af0a2880aa85010
nhlong2701 <nguyenhailong2701@gmail.com>
2025-11-06 18:00:18 +0100

    fix pre-commit for info.py

--- pypsa-spice-vis/scripts/info.py
+++ pypsa-spice-vis/scripts/info.py
@@ -2,20 +2,22 @@
 
 # SPDX-License-Identifier: GPL-2.0-or-later
 
-"""
-Create info page under General section showing technological nomenclature used in
-PyPSA-SPICE.
+"""Create an info page under the General section.
+
+This page explains the technological nomenclature used in PyPSA-SPICE.
 """
 
-import streamlit as st
 import os
+
 import pandas as pd
+import streamlit as st
 
 st.title("PyPSA-SPICE technological nomenclature")
 st.divider()
 
 with open(
-    os.path.join(st.session_state.current_dir, "setting/tech_mapping.csv"), "r"
+    os.path.join(st.session_state.current_dir, "setting/tech_mapping.csv"),
+    encoding="utf-8",
 ) as file:
     tech_info = pd.read_csv(file)[["original_names", "nice_names"]]
     tech_info.columns = ["Abbreviations", "Full names"]
