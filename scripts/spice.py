0fffcbf351f8200937875be085583ef490ad3774
nhlong2701 <nguyenhailong2701@gmail.com>
2025-12-05 09:50:50 +0100

    black formatting all files

--- scripts/spice.py
+++ scripts/spice.py
@@ -1,23 +1,104 @@
 def spice():
-    print("Spicing up the network...",'\n','\n',)
-    print('                         **********---                     ',end='\n',flush=True)  
-    print('                     ***************---.                   ',end='\n',flush=True)  
-    print('                  *** *************:-----   ---            ',end='\n',flush=True)  
-    print('                **  ****************----                   ',end='\n',flush=True)  
-    print('               ** ******************--                     ',end='\n',flush=True)  
-    print('              *  ***********=                              ',end='\n',flush=True)  
-    print('             ** *********                                  ',end='\n',flush=True)  
-    print('             *  ********                                   ',end='\n',flush=True)  
-    print('            ** ********                       **           ',end='\n',flush=True)  
-    print('            ** *******                        =**          ',end='\n',flush=True)  
-    print('            ** *******                       ****          ',end='\n',flush=True)  
-    print('             **********                     +****          ',end='\n',flush=True)  
-    print('             ***********                   ******          ',end='\n',flush=True)  
-    print('              ***********                *******           ',end='\n',flush=True)  
-    print('               *************          *********            ',end='\n',flush=True)  
-    print('                 *****************************             ',end='\n',flush=True)  
-    print('                   *************************               ',end='\n',flush=True)  
-    print('                     *********************                 ',end='\n',flush=True)   
-    print('                          ***********                      ',end='\n',flush=True)
-    
+    print(
+        "Spicing up the network...",
+        "\n",
+        "\n",
+    )
+    print(
+        "                         **********---                     ",
+        end="\n",
+        flush=True,
+    )
+    print(
+        "                     ***************---.                   ",
+        end="\n",
+        flush=True,
+    )
+    print(
+        "                  *** *************:-----   ---            ",
+        end="\n",
+        flush=True,
+    )
+    print(
+        "                **  ****************----                   ",
+        end="\n",
+        flush=True,
+    )
+    print(
+        "               ** ******************--                     ",
+        end="\n",
+        flush=True,
+    )
+    print(
+        "              *  ***********=                              ",
+        end="\n",
+        flush=True,
+    )
+    print(
+        "             ** *********                                  ",
+        end="\n",
+        flush=True,
+    )
+    print(
+        "             *  ********                                   ",
+        end="\n",
+        flush=True,
+    )
+    print(
+        "            ** ********                       **           ",
+        end="\n",
+        flush=True,
+    )
+    print(
+        "            ** *******                        =**          ",
+        end="\n",
+        flush=True,
+    )
+    print(
+        "            ** *******                       ****          ",
+        end="\n",
+        flush=True,
+    )
+    print(
+        "             **********                     +****          ",
+        end="\n",
+        flush=True,
+    )
+    print(
+        "             ***********                   ******          ",
+        end="\n",
+        flush=True,
+    )
+    print(
+        "              ***********                *******           ",
+        end="\n",
+        flush=True,
+    )
+    print(
+        "               *************          *********            ",
+        end="\n",
+        flush=True,
+    )
+    print(
+        "                 *****************************             ",
+        end="\n",
+        flush=True,
+    )
+    print(
+        "                   *************************               ",
+        end="\n",
+        flush=True,
+    )
+    print(
+        "                     *********************                 ",
+        end="\n",
+        flush=True,
+    )
+    print(
+        "                          ***********                      ",
+        end="\n",
+        flush=True,
+    )
+
+
 spice()
