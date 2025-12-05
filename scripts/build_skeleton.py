2650d49922e2ac04d301ff929a43fe447c667d64
samarthiith <samarth.iith@gmail.com>
2025-12-05 10:01:35 +0100

    cleanup to pass pre-commit

--- scripts/build_skeleton.py
+++ scripts/build_skeleton.py
@@ -8,29 +8,6 @@ Generate skeleton CSV files for setting up an energy system modelling.
 It creates templates for buses, generators, storage, loads, interconnectors,
 and converter links for power, industry and transport sectors.
 """
-                                                
-                                                        
-                                                        
-########                         **********---                       
-########                     ***************---.                     
-########                  *** *************:-----   ---              
-########                **  ****************----                     
-########               ** ******************--                       
-########              *  ***********=                                
-########             ** *********                                    
-########             *  ********                                     
-########            ** ********                       **             
-########            ** *******                        =**            
-########            ** *******                       ****            
-########             **********                     +****            
-########             ***********                   ******            
-########              ***********                *******             
-########               *************          *********              
-########                 *****************************               
-########                   *************************                 
-########                     *********************                    
-########                          ***********                        
-                                                 
 
 import glob
 import itertools
@@ -742,7 +719,7 @@ def create_interconnector(
     for column in fill_columns:
         intercon_df[column] = "Please fill here"
 
-    # ================================ 🌶 Finalize and save 🌶 ===============================
+    # ======================== 🌶 Finalize and save 🌶 ===================
     intercon_df = intercon_df.set_index(["country", "link"]).sort_values(
         by=["bus0", "bus1"]
     )
--- scripts/spice.py
+++ scripts/spice.py
@@ -1,23 +1,108 @@
+"""Module for displaying PyPSA-SPICE ASCII art."""
+
+
 def spice():
-    print("Spicing up the network...",'\n','\n',)
-    print('                         **********---                     ',end='\n',flush=True)  
-    print('                     ***************---.                   ',end='\n',flush=True)  
-    print('                  *** *************:-----   ---            ',end='\n',flush=True)  
-    print('                **  ****************----                   ',end='\n',flush=True)  
-    print('               ** ******************--                     ',end='\n',flush=True)  
-    print('              *  ***********=                              ',end='\n',flush=True)  
-    print('             ** *********                                  ',end='\n',flush=True)  
-    print('             *  ********                                   ',end='\n',flush=True)  
-    print('            ** ********                       **           ',end='\n',flush=True)  
-    print('            ** *******                        =**          ',end='\n',flush=True)  
-    print('            ** *******                       ****          ',end='\n',flush=True)  
-    print('             **********                     +****          ',end='\n',flush=True)  
-    print('             ***********                   ******          ',end='\n',flush=True)  
-    print('              ***********                *******           ',end='\n',flush=True)  
-    print('               *************          *********            ',end='\n',flush=True)  
-    print('                 *****************************             ',end='\n',flush=True)  
-    print('                   *************************               ',end='\n',flush=True)  
-    print('                     *********************                 ',end='\n',flush=True)   
-    print('                          ***********                      ',end='\n',flush=True)
-    
+    """Print a fun spice ASCII art to the console."""
+    print(
+        "Spicing up the network...",
+        "\n",
+        "\n",
+    )
+    print(
+        "                         **********---                     ",
+        end="\n",
+        flush=True,
+    )
+    print(
+        "                     ***************---.                   ",
+        end="\n",
+        flush=True,
+    )
+    print(
+        "                  *** *************:-----   ---            ",
+        end="\n",
+        flush=True,
+    )
+    print(
+        "                **  ****************----                   ",
+        end="\n",
+        flush=True,
+    )
+    print(
+        "               ** ******************--                     ",
+        end="\n",
+        flush=True,
+    )
+    print(
+        "              *  ***********=                              ",
+        end="\n",
+        flush=True,
+    )
+    print(
+        "             ** *********                                  ",
+        end="\n",
+        flush=True,
+    )
+    print(
+        "             *  ********                                   ",
+        end="\n",
+        flush=True,
+    )
+    print(
+        "            ** ********                       **           ",
+        end="\n",
+        flush=True,
+    )
+    print(
+        "            ** *******                        =**          ",
+        end="\n",
+        flush=True,
+    )
+    print(
+        "            ** *******                       ****          ",
+        end="\n",
+        flush=True,
+    )
+    print(
+        "             **********                     +****          ",
+        end="\n",
+        flush=True,
+    )
+    print(
+        "             ***********                   ******          ",
+        end="\n",
+        flush=True,
+    )
+    print(
+        "              ***********                *******           ",
+        end="\n",
+        flush=True,
+    )
+    print(
+        "               *************          *********            ",
+        end="\n",
+        flush=True,
+    )
+    print(
+        "                 *****************************             ",
+        end="\n",
+        flush=True,
+    )
+    print(
+        "                   *************************               ",
+        end="\n",
+        flush=True,
+    )
+    print(
+        "                     *********************                 ",
+        end="\n",
+        flush=True,
+    )
+    print(
+        "                          ***********                      ",
+        end="\n",
+        flush=True,
+    )
+
+
 spice()
