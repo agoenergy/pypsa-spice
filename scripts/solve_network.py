7c0bca61bff3c8f6f87d1b66902654d7b07884dc
nhlong2701 <nguyenhailong2701@gmail.com>
2026-01-09 13:01:52 +0100

    renaming function to specify constraint for power sector only

--- scripts/solve_network.py
+++ scripts/solve_network.py
@@ -22,7 +22,7 @@ import pypsa
 from _helpers import configure_logging, load_scenario_config
 from custom_constraints import (
     add_energy_independence_constraint,
-    add_maximum_generation_constraint,
+    add_maximum_power_generation_constraint,
     add_reserve_margin,
     add_storage_constraints,
     capacity_factor_constraint,
@@ -87,7 +87,7 @@ def extra_functionality_linopt(
 
         # Power generation constraint
         if country_constraints["maximum_generation_constraint"].get("activate", False):
-            add_maximum_generation_constraint(
+            add_maximum_power_generation_constraint(
                 network,
                 country=country,
                 year=year,
