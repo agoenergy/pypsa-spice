db38dde31403d4da5c53e38a7249a178ba4a6ace
RichChang963 <rich.chang.963@gmail.com>
2026-02-20 11:50:06 +0100

    fix indexing issues in all hourly storage flow tables

--- scripts/_post_analysis.py
+++ scripts/_post_analysis.py
@@ -2047,9 +2047,9 @@ class OutputTables(Plots):
                 .set_index("snapshot")
             )
             flow["country"] = country
-            flow = flow.set_index("country", append=True)
-            all_country_df = pd.concat([all_country_df, flow], axis=1)
-        return all_country_df
+            all_country_df = pd.concat([all_country_df, flow], axis=0)
+
+        return all_country_df.set_index("country", append=True)
 
     # Electricity hourly load by sector (MW)
     def pow_elec_load_by_sector_hourly(self, year: int, nth_hour: int) -> pd.DataFrame:
